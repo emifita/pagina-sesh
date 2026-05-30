a7183cf0fd12ba91aba7488028d31dcb0d4d57e9
emifita <renanemifita444@gmail.com>
2026-05-30 11:23:53 -0300

    Adding backend application

--- src/api/youtube-api.js
+++ src/api/youtube-api.js
@@ -1,6 +1,9 @@
+import * as dotenv from "../../node_modules/dotenv"
+dotenv.config()
+
 //Using the YouTube Data API v3 
 export default async function dataYT(music){
-    const apiKey = document.getElementById("apiKey").value;
+    const apiKey = process.env.YOUTUBE_API_KEY
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoCategoryId=10&maxResults=5&q=${encodeURIComponent(music)}&key=${apiKey}`
     
 
